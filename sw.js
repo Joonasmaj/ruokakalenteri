27a624d0b14430cec27d3d601a66cad35df30fa0
Joonasmaj <joonasm02@gmail.com>
2026-01-01 23:33:45 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = "mealcal-cache-v2";
+const ASSETS = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
